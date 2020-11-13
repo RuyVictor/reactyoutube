067ea4dd0cb1dea7de39bbb1e40af46567f074d2
Ruy Victor <ruyvictor@hotmail.com.br>
2020-11-12 21:26:07 -0300

    ícone de usuário

--- src/components/videocontainer/VideoContainer.js
+++ src/components/videocontainer/VideoContainer.js
@@ -1,6 +1,5 @@
 import './VideoContainer.css';
-import {Button, TextField, IconButton, InputAdornment, Typography, Divider} from '@material-ui/core';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {Typography, Divider} from '@material-ui/core';
 
 function VideoContainer() {
   let rows = []
@@ -8,12 +7,17 @@ function VideoContainer() {
       rows.push(
         <div className="video-card">
           <img src={thumbnail_main} className="video-thumbnail" alt="logo" />
-          <Typography variant="button" display="block" gutterBottom>
-            Titulo
-          </Typography>
-          <Typography style={{fontSize: 12}} variant="caption" display="block" gutterBottom>
-            Descrição
-          </Typography>
+          <div style={{display: 'flex'}}>
+            <img src={user} className="user-icon" alt="logo" />
+            <div style={{display: 'block'}}>
+            <Typography variant="button" display="block" gutterBottom>
+              Titulo
+            </Typography>
+            <Typography style={{fontSize: 12}} variant="caption" display="block" gutterBottom>
+              Descrição
+            </Typography>
+            </div>
+          </div> 
         </div>
       )
     }
@@ -22,18 +26,23 @@ function VideoContainer() {
       for(let i=0; i<6; i++){
         covid.push(
           <div className="video-card">
-            <img src={thumbnail_covid} className="video-thumbnail" alt="logo" />
+          <img src={thumbnail_covid} className="video-thumbnail" alt="logo" />
+          <div style={{display: 'flex'}}>
+            <img src={user} className="user-icon" alt="logo" />
+            <div style={{display: 'block'}}>
             <Typography variant="button" display="block" gutterBottom>
               Titulo
             </Typography>
             <Typography style={{fontSize: 12}} variant="caption" display="block" gutterBottom>
               Descrição
             </Typography>
-          </div>
+            </div>
+          </div> 
+        </div>
         )
       }
   return (
-    <div style={{padding: 25}}>
+    <div className="all-container" style={{padding: 25}}>
       <div className="ads-container">
         <img src={ads} className="ads-thumbnail" alt="ads" />
         <img src={adsSales} className="ads-sales-thumbnail" alt="ads" />
@@ -55,5 +64,6 @@ const ads = 'https://revendedor.club/wp-content/uploads/2019/10/Cartao-de-credit
 const adsSales = 'https://i1.wp.com/www.dci.com.br/wp-content/uploads/2020/10/shutterstock_1704856861-2-min.jpg?fit=1280%2C720&ssl=1';
 const thumbnail_main = 'https://i.ytimg.com/vi/658c0gylxQo/maxresdefault.jpg';
 const thumbnail_covid = 'https://www.thelocalvoice.net/oxford/wp-content/uploads/2020/08/MississippiCoronavirus.jpg';
+const user = 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/954d6b55320395.597fe4de4c7ad.png';
 
 export default VideoContainer;
